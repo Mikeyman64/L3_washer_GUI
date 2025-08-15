f1f8455599ab8a587dbebc65f77e8a612dc91242
Mikeyman64 <43762644+Mikeyman64@users.noreply.github.com>
2025-08-15 12:21:34 -0400

    Update run_washer_main.py

--- run_washer_main.py
+++ run_washer_main.py
@@ -127,7 +127,7 @@ class Runner(MDApp):
         driver = os.getenv('DRIVER')
         server = os.getenv('SERVER')
         user = os.getenv('UID')
-        password = os.getenv('PWD')
+        password = 'aztmvcjfrizkcpdcehky'#os.getenv('PWD')
         database = os.getenv('DATABASE')
 
         # Connect to the database using pyodbc
